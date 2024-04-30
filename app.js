95acd03e7b2ebf4081d35c872c7121ab44aabbf6
Nikita8Sannikov <Ak-4t7@mail.ru>
2024-04-30 12:37:10 +0600

    add slider logic

--- app.js
+++ app.js
@@ -48,3 +48,26 @@ function format(formatMode){
 }
 
 
+
+
+slidesPlugin = (activeSlide = 0) =>{
+    const slides = document.querySelectorAll('.gallery-slide')
+
+    slides[activeSlide].classList.add('active')
+
+        slides.forEach( (slide) =>{
+            slide.addEventListener('click', () =>{
+                 clearActiveClasses()
+        
+                slide.classList.add('active')
+            })
+        })
+
+    function clearActiveClasses () {
+        slides.forEach( (slide) =>{
+            slide.classList.remove('active')
+        })
+    }
+}
+
+slidesPlugin(2)
