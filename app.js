bfbc0fff049c5d3c16e40541d0078c528253cefc
Nikita8Sannikov <Ak-4t7@mail.ru>
2024-05-06 10:46:56 +0600

    add BEM

--- app.js
+++ app.js
@@ -51,21 +51,21 @@ function format(formatMode){
 
 
 slidesPlugin = (activeSlide = 0) =>{
-    const slides = document.querySelectorAll('.gallery-slide')
+    const slides = document.querySelectorAll('.gallery__slide')
 
-    slides[activeSlide].classList.add('active')
+    slides[activeSlide].classList.add('--active')
 
         slides.forEach( (slide) =>{
             slide.addEventListener('click', () =>{
                  clearActiveClasses()
         
-                slide.classList.add('active')
+                slide.classList.add('--active')
             })
         })
 
     function clearActiveClasses () {
         slides.forEach( (slide) =>{
-            slide.classList.remove('active')
+            slide.classList.remove('--active')
         })
     }
 }
