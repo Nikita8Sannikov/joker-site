523d32586615e37b95f80daf8660e88307df785f
Nikita8Sannikov <Ak-4t7@mail.ru>
2024-05-14 10:06:05 +0600

    add random phrases to sideSlides

--- app.js
+++ app.js
@@ -18,6 +18,34 @@ const dropZone = document.querySelector('.drop-zone')
 const premier = new Date(2024, 9, 4, 19, 30)
 const rest = (premier - new Date()) / 1000 / 60 / 60 / 24
 
+const phrases = [
+  'Я вдруг понял, что большинство людей не слушают. Они просто ждут своей очереди снова заговорить.',
+  'Я думал, что моя жизнь — трагедия. Но сейчас понял, что это чёртова комедия.',
+  'Я надеюсь, что моя смерть будет иметь больше смысла, чем моя жизнь.',
+  'Худшее свойство психического заболевания состоит в том, что люди ожидают, что вы будете вести себя так, будто его у вас нет.',
+  'Что ты получишь, если смешаешь психически больного одиночку с обществом, которое игнорирует его и обращается с ним как с мусором? Ты получишь то, что заслужил.',
+  'Комедия субъективна, Мюррей, разве не так говорят? Вы все, система, которая так много знает, вы решаете, что хорошо, а что плохо. Точно так же, вы решаете, что смешно, а что нет.',
+  `— У вас были негативные мысли?
+  — Все, что у меня есть, — это негативные мысли.`,
+  'Всю жизнь я не знал, существую ли я. Существую! И люди начинают замечать меня.',
+  'Это я... или весь мир сходит с ума?',
+  'Моя мама всегда говорила мне: «Улыбнись и сделай счастливое лицо». Говорила... мое предназначение — приносить радость и счастье в этот мир.',
+  `— Так, то есть дело в том, что вы — псих? В этом ваше оправдание убийства трёх человек?
+  — Нет, они слишком фальшиво пели, чтобы иметь право жить.`,
+  `— Эй, Мюрей, хотел попросить.
+  — Да?
+  — Будешь объявлять, назови меня Джокер.`,
+  ]
+
+
+
+  function randomInteger(min, max) {
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand)
+  }
+
+  console.log(phrases[randomInteger(0, phrases.length)])
+ 
 function createFormatter(restTime) {
   return function (formatType) {
     let days = Math.floor(restTime)
@@ -194,7 +222,7 @@ function createSideSlide(imageUrl) {
   sideSlide.classList.add('sidebar__slide')
   RGBaster.colors(imageUrl, {
     // Не учитывать белый цвет
-    exclude: ['rgb(255,255,255)'],
+    // exclude: ['rgb(255,255,255)'], 
     success: function(payload) {
       sideSlide.style.background = `linear-gradient(229.99deg, ${payload.dominant} -26%, ${payload.secondary} 145%)`
     }
@@ -204,6 +232,6 @@ function createSideSlide(imageUrl) {
   // sidebar.insertBefore(sideSlide, sidebar.firstChild)
 
   const pSideSlide = document.createElement('p');
-  pSideSlide.textContent = 'Цитата Джокера'
+  pSideSlide.textContent = phrases[randomInteger(0, phrases.length)]
   sideSlide.appendChild(pSideSlide)
 }
