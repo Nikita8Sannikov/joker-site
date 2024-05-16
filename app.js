ae7e1d101008e020b69a36800e52d9b8cd21c56c
Nikita8Sannikov <Ak-4t7@mail.ru>
2024-05-16 22:22:35 +0600

    make module of timer

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import {slidesPlugin} from './slider/slider.js'
-let mode = "full"
-// const now = new Date()
-const output = document.querySelector(".timer__output")
+import {update, bindMode} from './timer/timer.js'
+
+
+
 const fullBtn = document.getElementById("full")
 const dateBtn = document.getElementById("date")
 const timeBtn = document.getElementById("time")
@@ -17,8 +18,7 @@ let slidesCount = mainSlide.querySelectorAll("div").length
 const dropZone = document.querySelector(".drop-zone")
 const fileInput = document.getElementById("file-input")
 
-// const premier = new Date(2024, 9, 4, 19, 30)
-// const rest = (premier - new Date()) / 1000 / 60 / 60 / 24
+
 
 const phrases = [
   "Я вдруг понял, что большинство людей не слушают. Они просто ждут своей очереди снова заговорить.",
@@ -45,75 +45,16 @@ function randomInteger(min, max) {
 }
 
 
-
-function getRest() {
-  const premier = new Date(2024, 9, 4, 19, 30)
-  return (premier - new Date()) / 1000 / 60 / 60 / 24
-}
-
-function createFormatter() {
-  return function (formatType) {
-    const restTime = getRest()
-    let days = Math.floor(restTime)
-    let hours = Math.floor((restTime - days) * 24)
-    let minutes = Math.floor(((restTime - days) * 24 - hours) * 60)
-    let seconds = Math.floor(
-      (((restTime - days) * 24 - hours) * 60 - minutes) * 60
-    )
-    let fullHours = days * 24 + hours
-
-    switch (formatType) {
-      case "days":
-        return days + ` дней `
-      case "hours":
-        return hours + ` часов `
-      case "minutes":
-        return minutes + ` минут `
-      case "seconds":
-        return seconds + ` секунд`
-      case "fullHours":
-        return fullHours + ` часов `
-    }
-  }
-}
-
-let formatTime = createFormatter()
-
-function bindMode(name) {
-  return function () {
-    mode = name
-    update()
-  }
-}
-
+//Обрабатываем нажатия кнопок
 fullBtn.addEventListener("click", bindMode("full"))
 dateBtn.addEventListener("click", bindMode("date"))
 timeBtn.addEventListener("click", bindMode("time"))
 
-function update() {
-  output.textContent = format(mode)
-}
-
+//Вызываем таймер
 setInterval(update, 1000)
 update()
 
-function format(formatMode) {
-  switch (formatMode) {
-    case "full":
-      return (
-        formatTime("days") +
-        formatTime("hours") +
-        formatTime("minutes") +
-        formatTime("seconds")
-      )
-    case "date":
-      return formatTime("days")
-    case "time":
-      return (
-        formatTime("fullHours") + formatTime("minutes") + formatTime("seconds")
-      )
-  }
-}
+
 
 
 //Вызываем Слайдер
---
+++ timer/timer.js
@@ -0,0 +1,65 @@
+let mode = "full"
+export const update = () => {
+    const output = document.querySelector(".timer__output")
+    
+    function getRest() {
+      const premier = new Date(2024, 9, 4, 19, 30)
+      return (premier - new Date()) / 1000 / 60 / 60 / 24
+    }
+    
+    function createFormatter() {
+      return function (formatType) {
+        const restTime = getRest()
+        let days = Math.floor(restTime)
+        let hours = Math.floor((restTime - days) * 24)
+        let minutes = Math.floor(((restTime - days) * 24 - hours) * 60)
+        let seconds = Math.floor(
+          (((restTime - days) * 24 - hours) * 60 - minutes) * 60
+        )
+        let fullHours = days * 24 + hours
+    
+        switch (formatType) {
+          case "days":
+            return days + ` дней `
+          case "hours":
+            return hours + ` часов `
+          case "minutes":
+            return minutes + ` минут `
+          case "seconds":
+            return seconds + ` секунд`
+          case "fullHours":
+            return fullHours + ` часов `
+        }
+      }
+    }
+    
+  let formatTime = createFormatter()
+  
+  function format(formatMode) {
+    switch (formatMode) {
+      case "full":
+        return (
+          formatTime("days") +
+          formatTime("hours") +
+          formatTime("minutes") +
+          formatTime("seconds")
+        )
+      case "date":
+        return formatTime("days")
+      case "time":
+        return (
+          formatTime("fullHours") + formatTime("minutes") + formatTime("seconds")
+        )
+    }
+  }
+  
+  output.textContent = format(mode)
+  }
+
+  export function bindMode(name) {
+    return function () {
+      mode = name
+      update()
+    }
+  }
+  
